f0a2db2577229da8e12f5fa196212e136040b9b6
emre-backendguru <akis@backendguru.com>
2025-10-31 08:26:56 +0300

    user-service eklendi

---
+++ api-gateway/src/main/java/com/beguru/service/api_gateway/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.beguru.service.api_gateway;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http
+                .authorizeExchange(exchanges -> exchanges
+                        .pathMatchers("/actuator/**").permitAll() // Örnek: Actuator uç noktalarına izin ver
+                        // Gerekirse belirli yol korumalarını tanımlayın veya varsayılan olarak kimlik doğrulamayı kullanın
+                        .anyExchange().authenticated()
+                        //.anyExchange().permitAll()
+                )
+                //.oauth2Login(Customizer.withDefaults()) // Keycloak aracılığıyla login akışını başlatmak için
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(Customizer.withDefaults())); // Bearer belirteçlerini (JWT'ler) doğrulamak için
+        http.csrf(csrf -> csrf.disable()); // Durumsuz API'ler için yaygın, CSRF ihtiyaçlarını dikkatlice değerlendirin
+        http.cors(Customizer.withDefaults());
+        return http.build();
+    }
+}
